c1a8ea04458ded605fdd09a59ea986491a595b3b
oeccsy <oeccsy@users.noreply.github.com>
2021-02-03 18:22:33 +0900

    스크립트로 오브젝트의 위치/회전 조절
    -여러 클래스를 만들고 클래스들끼리의 상호작용을 구현하였다.
    
    -스크립트로 오브젝트의 위치, 회전 상태를 바꾸는 과정을 구현하였다.

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int damage = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            var mon = FindObjectOfType<Monster>();
+            if(Attack(mon))
+            {
+                Destroy(mon.gameObject);
+                Debug.Log("몬스터가 죽었다!");
+            }
+            else
+            {
+                Debug.Log($"몬스터가 죽지 않았다 ! 남은 체력 :: {mon.hp}");
+            }
+        }
+    }
+
+    public bool Attack(Monster monster)
+    {
+        monster.hp -= damage;
+        return monster.hp <= 0;
+
+    }
+}
---
+++ Assets/Script/TransformController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformController : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Rotatef(new Vector3(0f, (Mathf.Cos(timer)*0.5f+0.5f)*360f, 0f));
+    }
+
+
+
+
+
+    //각 변환
+    public void RotateGameObject(Vector3 rotation)
+    {
+        transform.rotation = Quaternion.Euler(rotation); //Quaternion 대신 Euler 체계를 사용
+                                                        //해당 각도상태로 고정
+
+    }
+
+    public void Rotatef(Vector3 rotation)
+    {
+        transform.Rotate(rotation); //매 프레임마다 해당 방향으로 회전한다.
+    }
+
+
+
+    //위치 변환
+
+    private void MovePosition(Vector3 newPos)
+    {
+        transform.position = newPos; //transform.position 은 MonoBehaviour 로부터 상속받은 프로퍼티
+                                    //위치를 매개변수로 받는다. 해당 위치에 고정된다.
+    }
+
+
+    private void MoveTranslate(Vector3 moveVector)
+    {
+        transform.Translate(moveVector); //Translate 는 프레임마다 원래 위치에서 vector만큼 움직인다.
+                                        //이동할 방향과 속력을 매개변수로 받는것과 같다.
+    }
+}
