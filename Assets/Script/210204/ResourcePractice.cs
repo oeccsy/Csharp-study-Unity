543893d335f33e4afedafc349dff167ca61d75be
oeccsy <oeccsy@users.noreply.github.com>
2021-02-04 21:58:52 +0900

    Resources, 프리팹
    - Resources 폴더를 이용하여 프로젝트에 포함된 에셋을 불러오는 방법 확인
    
    - 프리팹 기초 사용법 확인

---
+++ Assets/Script/210204/ResourcePractice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcePractice : MonoBehaviour
+{
+    public GameObject prefab;
+    public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefab = Resources.Load<GameObject>("Cube");
+        material = Resources.Load<Material>("Black");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
