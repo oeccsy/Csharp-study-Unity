97290e0022105aa4a566aa3556a4125007073fbd
oeccsy <oeccsy@users.noreply.github.com>
2021-02-04 17:02:40 +0900

    스크립트를 이용하여 게임 오브젝트 생성 및 파괴
    - GameObject() 를 이용하여 게임 오브젝트를 생성하는 코드를 구현
    
    - Destroy() 를 이용하여 게임 오브젝트를 파괴하는 코드를 구현
    
    -  AddComponent<>() 를 이용하여 컴포넌트를 부착하는 코드를 구현
    
    - 스크립트를 이용하여 오브젝트의 이름을 바꾸는 코드를 구현

---
+++ Assets/Script/210204/CreateObjScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObjScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        new GameObject(); //동적으로 빈 오브젝트 생성
+
+        GameObject newObj = new GameObject(); //GameObject 타입의 newObject 변수를 생성
+        newObj.AddComponent<DestroyObjScript> (); //newObject에 새로운 컴포넌트 부착
+        newObj.name = "Add Destroy Component";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
