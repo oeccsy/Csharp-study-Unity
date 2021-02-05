6aef549c94039d7e79d5258eee98fd047f61a8bc
oeccsy <oeccsy@users.noreply.github.com>
2021-02-05 20:23:04 +0900

    Collider
    -Collider를 이용하여 충돌을 감지하는 코드 구현

---
+++ Assets/Script/210205/ColliderScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision) //Is Trigger 가 체크되지 않았을때 사용
+    {
+        Debug.Log("충돌 시작!");
+    }
+
+    private void OnCollisionStay(Collision collision) //Is Trigger 가 체크되지 않았을때 사용
+    {
+        Debug.Log("충돌 중");
+    }
+
+    private void OnCollisionExit(Collision collision) //Is Trigger 가 체크되지 않았을때 사용
+    {
+        Debug.Log("충돌 끝");
+
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("트리거 시작");
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        Debug.Log("트리거 작동중");
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        Debug.Log("트리거 끝");
+    }
+}
