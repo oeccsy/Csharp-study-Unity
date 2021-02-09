0572398671bb3fb377cfc195fde0e1266557c508
oeccsy <oeccsy@users.noreply.github.com>
2021-02-09 23:58:37 +0900

    UGUI 기초
    - 캔버스를 생성하여 버튼을 배치하였다.
    
    - 버튼을 배치하고 함수를 호출하는 기능을 활용하여 Scene을 이동하거나 게임을 종료하는 코드를 구현하였다.

---
+++ Assets/Script/210209/UGUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UGUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickNewGame()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
