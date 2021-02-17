9341e72852d789b40a273599f7a0437d4e11396c
oeccsy <oeccsy@users.noreply.github.com>
2021-02-17 18:13:57 +0900

    조건에 따른 다양한 애니메이션 적용
    - 3가지 애니메이션이 조건에 따라 동작하는 코드를 구현하였다.
    
    - 캐릭터(상자)가 가만히 있는 경우 기본상태인 Idle 애니메이션을 적용하였다.
    
    - 캐릭터가 움직일 때 뚜껑이 열리는 Open 애니메이션을 적용하였다.
    
    - Shift를 누르며 이동할 경우 사람처럼 걷는듯한 Walk 애니메이션을 적용하였다.

--- Assets/Script/210217/Player.cs
+++ Assets/Script/210217/Player.cs
@@ -8,12 +8,15 @@ public class Player : MonoBehaviour
     public float speed;
     float hAxis;
     float vAxis;
+    bool wDown;
 
     Vector3 moveVec;
 
-    void Start()
+    Animator anim;
+
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>(); //자식 오브젝트에 있는 컴포넌트를 가지고온다.
     }
 
     // Update is called once per frame
@@ -21,9 +24,13 @@ public class Player : MonoBehaviour
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * Time.deltaTime;
+
+        anim.SetBool("isOpen", moveVec != Vector3.zero); //SetBool() 은 파라메터 값을 설정할 수 있다.
+        anim.SetBool("isWalk", wDown);
     }
 }
