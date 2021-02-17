e5a426253f6e4c7d00cad1dd4ea02d03b4c9cbcc
oeccsy <oeccsy@users.noreply.github.com>
2021-02-17 23:25:01 +0900

    캐릭터에 Jump기능 추가 및 애니메이션 수정
    -캐릭터에 Jump기능을 추가하였다.
    
    - 점프중에 상자 뚜껑이 열리도록 하였다.
    
    - 점프중에 다시 점프할 수 없도록 bool변수를 추가하여 점프중에는 다시 점프 할 수 없도록 하였다.
    
    - 캐릭터가 이동중에 Walk 애니메이션이 적용되고 Shift를 누를경우 뚜껑이 열리는 애니메이션이 적용되도록 수정하였다.

--- Assets/Script/210217/Player.cs
+++ Assets/Script/210217/Player.cs
@@ -9,26 +9,44 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+    bool isJump;
 
     Vector3 moveVec;
 
+    Rigidbody rigid; // 여기있는 변수들은 Awake에서 초기화시켜준다!
     Animator anim;
 
     void Awake()
     {
-        anim = GetComponentInChildren<Animator>(); //자식 오브젝트에 있는 컴포넌트를 가지고온다.
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>(); //자식 오브젝트에 있는 컴포넌트를 가지고온다.
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetInput(); //입력을 받는다.
+        Move(); //입력받은 값에 따라 움직인다.
+        Turn(); //움직이는 방향을 바라보도록 한다.
+        Jump();
+
+    }
+
+    
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
-        wDown = Input.GetButton("Walk");
+        wDown = Input.GetButton("Shift");
+        jDown = Input.GetButtonDown("Jump");
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        if(wDown == false)
+        if (wDown == false)
         {
             transform.position += moveVec * speed * Time.deltaTime;
         }
@@ -37,12 +55,32 @@ public class Player : MonoBehaviour
             transform.position += moveVec * speed * 0.3f * Time.deltaTime;
         }
 
-        transform.position += moveVec * speed * Time.deltaTime;
+        anim.SetBool("isWalk", moveVec != Vector3.zero); //SetBool() 은 파라메터 값을 설정할 수 있다.
+        anim.SetBool("isOpen", wDown);
+    }
 
-        anim.SetBool("isOpen", moveVec != Vector3.zero); //SetBool() 은 파라메터 값을 설정할 수 있다.
-        anim.SetBool("isWalk", wDown);
-
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec); //본인이 향하고 있는 위치를 바라보게 만드는 코드
+    }
 
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * 7, ForceMode.Impulse); //즉발적인 힘을 가하는 경우 Impulse를 사용한다.
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }   
+    }
+
+    void OnCollisionEnter (Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
