f8c806d813516c6aea913381367c6c8434cd1481
oeccsy <oeccsy@users.noreply.github.com>
2021-02-20 17:48:46 +0900

    코드 수정
    - public GameObject gameObject; 의 경우
    gameObject 사용 시 오브젝트 자기 자신이 아닌
    다른 지정한 오브젝트로 덮어 씌워진다.
    
    - gameObject 대신 obj를 사용하는 코드로 수정하였다.

--- Assets/Script/210220/SetActiveScript.cs
+++ Assets/Script/210220/SetActiveScript.cs
@@ -10,17 +10,17 @@ public class SetActiveScript : MonoBehaviour
         
     }
 
-    public GameObject gameObject;
+    public GameObject obj;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonDown("Jump"))
         {
-            if (gameObject.activeSelf == true) 
-                gameObject.SetActive(false);
+            if (obj.activeSelf == true) 
+                obj.SetActive(false);
             else //오브젝트가 비활성화인경우 아래 코드 실행되지 않음.
-                gameObject.SetActive(true);
+                obj.SetActive(true);
         }
  
     }
