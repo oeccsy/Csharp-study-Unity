1dd1b6d06e096aed7e92d2c4441fe437baaefe33
oeccsy <oeccsy@users.noreply.github.com>
2021-02-20 17:34:47 +0900

    스크립트로 오브젝트 활성화/비활성화하기
    - gameObject.SetActive(false);
    gameObject.SetActive(true);
    를 통해 오브젝트를 비활성화/활성화 할 수 있다.
    
    - gameObject.activeSelf 값을 통해 현재 해당 오브젝트가 활성화 상태인지 비활성화 상태인지 알 수 있다.
    
    - 오브젝트가 비활성화 되면 스크립트도 비활성화 되기 때문에 비활성화된 오브젝트를 스크립트로 활성화 하고 싶으면 다른 오브젝트의 스크립트를 이용해야한다.

---
+++ Assets/Script/210220/SetActiveScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject gameObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if (gameObject.activeSelf == true) 
+                gameObject.SetActive(false);
+            else //오브젝트가 비활성화인경우 아래 코드 실행되지 않음.
+                gameObject.SetActive(true);
+        }
+ 
+    }
+}
