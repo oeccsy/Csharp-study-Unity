e19b1434b06145b1f50423cce7892fccc182163b
oeccsy <oeccsy@users.noreply.github.com>
2021-02-21 22:46:24 +0900

    Vector3 클래스
    - Vector3.MoveTowards()
    일반적인 이동이다 일정한 속도로 이동한다.
    현재위치, 목표위치, 속도를 매개변수로 받는다.
    
    현재위치에 transform.position 대신에
    new Vector3(0,0,0) 처럼 고정된 벡터를 넣으면
    매 프레임마다 (0,0,0)에서 다시 움직이기 시작하기 때문에
    오브젝트가 떠는것처럼 보인다.
    
    -Vector3.SmoothDamp()
    목표위치에 가까워지면 느리게 이동한다.
    현재위치, 목표위치, 참조속도, 속도를 매개변수로 받는다.
    
    카메라를 부드럽게 이동하고 싶을때 이용하면 좋을것 같다.
    
    - Vector3.Lerp()
    선형보간
    목표위치에 가까워질수록 느리게 이동한다.
    SmoothDamp와 비슷하다.
    현재위치, 목표위치, 속도를 매개변수로 받는다.
    
    - Vector3.Slerp()
    구면보간
    포물선을 그리며 이동한다.
    현재위치, 목표위치, 속도를 매개변수로 받는다.

---
+++ Assets/Script/210221/VectorScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Vector3 target;
+    Vector3 zero = Vector3.zero;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+            transform.position = Vector3.MoveTowards(transform.position, target, 0.1f); //일반적인 이동
+
+
+        if (Input.GetKey(KeyCode.R))
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref zero, 0.5f); //도착 직전 감속하며 이동
+
+        if (Input.GetKey(KeyCode.T))
+            transform.position = Vector3.Lerp(transform.position, target, 0.05f); //도착 직전 감속하며 이동
+
+        if (Input.GetKey(KeyCode.Y))
+            transform.position = Vector3.Slerp(transform.position, target, 0.05f); //포물선 이동
+
+        if (Input.GetKey(KeyCode.Q))  //원위치로 이동
+            transform.position = new Vector3(0, 1, 0);
+    }
+}
