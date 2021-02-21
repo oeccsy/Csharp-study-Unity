720057b6c49d8c5081a953a50699cf0e4af3542f
oeccsy <oeccsy@users.noreply.github.com>
2021-02-21 22:03:22 +0900

    Input 클래스
    - Input 클래스는 입력을 받고 싶을 때 사용하는 클래스
    
    - Input.GetKey-(KeyCode.Space) 계열과
    Input.GetButton-("ButtonName") 계열은
    누를 때/뗄 때/누르는 중 true
    아닌 경우 false를 반환
    
    - Get - Down은 누른 순간 1회
    Get - Up은 떼는 순간 1회
    Get - 는 누르는 동안 계속 true를 반환한다.
    
    - GetAxis-("") 계열은 float 값을 반환
    GetAxis("")는 -1과 1사이의 값을 반환하는데
    오래 누를수록 -1과 1값에 가까워진다.
    GetAxisRaw("")는 -1,0,1 3가지 값 중 하나를 반환한다.
    
    - GetAxis("Vertical")의 경우 -1에서 1사이의 값을 반환하고
    GetAxis("Jump")의 경우 0에서 1사이의 값을 반환한다.
    
    - GetAxis("")의 float 값 변화 속도는
    Edit -> Project setting -> Input Manager 에서
    Gravity, Sensitivity 값을 변경하여 조절할 수 있다.
    Sensitivity의 경우 0에서 1 또는 -1로 가는 속도,
    Gravity의 경우 0으로 돌아오는 속도이다.

---
+++ Assets/Script/210220/InputClass.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+            Debug.Log("GetKeyDown U");
+
+        if (Input.GetKeyUp(KeyCode.I))
+            Debug.Log("GetKeyUp I");
+
+        if (Input.GetKey(KeyCode.O))
+            Debug.Log("GetKey O");
+
+        /*
+
+        if (Input.GetButtonDown("Shift"))
+            Debug.Log(Input.GetButtonDown("Shift"));
+
+        if (Input.GetButtonUp("Shift"))
+            Debug.Log(Input.GetButtonUp("Shift"));
+
+        */
+
+        if (Input.GetButton("Shift"))
+            Debug.Log(Input.GetButton("Shift"));
+
+
+
+
+        //Debug.Log("GetAxisRaw : " + Input.GetAxisRaw("Horizontal") + "호라이즌탈 로");
+
+        //Debug.Log("GetAxis : " + Input.GetAxis("Vertical"));
+
+        Debug.Log("점프 : " + Input.GetAxis("Jump"));
+
+
+    }
+}
