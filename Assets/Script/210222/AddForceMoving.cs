df464638c7555614c7c7956a1e2a84e3d945e18c
oeccsy <oeccsy@users.noreply.github.com>
2021-02-22 23:46:10 +0900

    AddForce로 물체 움직이기
    - 오브젝트의 position이 아닌 AddForce를 이용하여 물체를 움직이는 코드를 구현하였다.
    
    - 4가지의 ForceMode중 ForceMode.Impulse를 이용하였다.
    
    - 움직임에 관성이 생기기 때문에 미끄러지는 듯한 연출에 사용하면 좋겠다.

---
+++ Assets/Script/210222/AddForceMoving.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceMoving : MonoBehaviour
+{
+    Rigidbody rigid; //선언
+
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>(); //초기화
+        //rigid.velocity = Vector3.right; //velocity : 현재 이동속도, 벡터계열
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            rigid.AddForce(new Vector3(1, 0, 0), ForceMode.Impulse);
+                //Vec 방향과 크기로 힘을 줌
+        }
+
+        /*
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            rigid.AddForce(new Vector3(1, 0, 0), ForceMode.Acceleration);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            rigid.AddForce(new Vector3(1, 0, 0), ForceMode.Force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            rigid.AddForce(new Vector3(-1, 0, 0), ForceMode.VelocityChange);
+        }
+
+        */
+
+    }
+
+    void FixedUpdate()
+    {
+        rigid.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")), ForceMode.Impulse);
+    }
+}
