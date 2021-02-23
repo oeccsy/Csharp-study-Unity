ba3438a62c44cf3a4991ca5b33f985cfb0742630
oeccsy <oeccsy@users.noreply.github.com>
2021-02-23 23:33:19 +0900

    오브젝트의 Material 변경하기
    - gameObject.GetComponent<MeshRenderer>();
    MeshRenderer 컴포넌트를 전달 받는 코드를 구현
    
    - 키를 입력하면 Material를 Black/White로 전환하는 코드를 구현

---
+++ Assets/Script/210223/MaterialChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour
+{
+    public Material black;
+    public Material white;
+
+    MeshRenderer mesh;
+
+    bool isBlack = false;
+
+    void Start()
+    {
+        mesh = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isBlack == false)
+        {
+            mesh.material = black;
+            isBlack = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            mesh.material = white;
+            isBlack = false;
+        }
+    }
+
+}
