f4b9ea0f1f4a1fa76638f444beb6f1060da679f3
oeccsy <oeccsy@users.noreply.github.com>
2021-02-24 23:33:03 +0900

    물체의 투명도 조절하기
    - 물체의 투명도를 조절하기 위해서는
    Material -> Surface Type을  Transparent로 변경해야 한다.
    
    - Color 클래스의 a값을 변경하여 투명도를 조절하는 코드를 구현하였다.

---
+++ Assets/Script/210224/MaterialChange2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange2 : MonoBehaviour
+{
+
+    MeshRenderer mesh;
+    Material material;
+    public Material black;
+    public Material white;
+
+    Color color;
+    
+    void Start()
+    {
+        mesh = gameObject.GetComponent<MeshRenderer>();
+        material = gameObject.GetComponent<MeshRenderer>().material; // mesh 초기화 이후 mesh.material; 와 같은 코드
+        color = gameObject.GetComponent<MeshRenderer>().material.color; //material 초기화 이후 material.color; 와 같은 코드
+    }
+
+    
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Z)) //투명도를 조절하는 코드
+        {
+            color.a -= 0.01f;
+        }
+
+        if(Input.GetKey(KeyCode.X))
+        {
+            color.a += 0.01f;
+        }
+
+        material.color = color;
+        Debug.Log("color : " + color);
+            
+    }
+}
